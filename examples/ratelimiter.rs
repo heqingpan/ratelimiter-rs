0e82e8cffba98f5261bb3011a4c0b4eadae01f1e
heqingpan <heqingpan@126.com>
2023-12-18 00:14:22 +0800

    RateLimiter add get inner value and  load by history value

--- examples/ratelimiter.rs
+++ examples/ratelimiter.rs
@@ -1,7 +1,6 @@
-
+use ratelimiter_rs::{now_millis, AtomicQpsLimiter, QpsLimiter, RateLimiter};
 use std::thread;
 use std::time::Duration;
-use ratelimiter_rs::{QpsLimiter,RateLimiter,now_millis, AtomicQpsLimiter};
 
 fn main() {
     println!("----- ratelimiter -----");
@@ -12,50 +11,47 @@ fn main() {
     atomic_qpslimiter();
 }
 
-fn ratelimiter(){
+fn ratelimiter() {
     let mut limiter = RateLimiter::new();
     let mut times = 0;
     for _ in 0..3000 {
         thread::sleep(Duration::from_millis(1));
-        if limiter.acquire(10,10) {
-            times +=1;
-            println!("time: {}",now_millis())
-        }
-        else{
+        if limiter.acquire(10, 10) {
+            times += 1;
+            println!("time: {}", now_millis())
+        } else {
             continue;
         }
     }
-    println!("time: {}, times: {}",now_millis(),&times);
+    println!("time: {}, times: {}", now_millis(), &times);
 }
 
-fn qpslimiter(){
+fn qpslimiter() {
     let mut limiter = QpsLimiter::new(10);
     let mut times = 0;
     for _ in 0..3000 {
         thread::sleep(Duration::from_millis(1));
         if limiter.acquire() {
-            times +=1;
-            println!("time: {}",now_millis())
-        }
-        else{
+            times += 1;
+            println!("time: {}", now_millis())
+        } else {
             continue;
         }
     }
-    println!("time: {}, times: {}",now_millis(),&times);
+    println!("time: {}, times: {}", now_millis(), &times);
 }
 
-fn atomic_qpslimiter(){
+fn atomic_qpslimiter() {
     let limiter = AtomicQpsLimiter::new(10);
     let mut times = 0;
     for _ in 0..3000 {
         thread::sleep(Duration::from_millis(1));
         if limiter.acquire() {
-            times +=1;
-            println!("time: {}",now_millis())
-        }
-        else{
+            times += 1;
+            println!("time: {}", now_millis())
+        } else {
             continue;
         }
     }
-    println!("time: {}, times: {}",now_millis(),&times);
+    println!("time: {}, times: {}", now_millis(), &times);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
-use std::{cmp, sync::{atomic::{AtomicI64, AtomicI32, Ordering::Relaxed}, Arc}};
-
+use std::{
+    cmp,
+    sync::{
+        atomic::{AtomicI32, AtomicI64, Ordering::Relaxed},
+        Arc,
+    },
+};
 
 pub fn now_millis() -> u128 {
     use std::time::SystemTime;
@@ -18,64 +23,76 @@ impl RateUnit {
     pub(crate) fn get_rate_to_conversion(&self) -> i32 {
         match self {
             RateUnit::Seconds => 1000,
-            RateUnit::Minutes => 60*1000,
+            RateUnit::Minutes => 60 * 1000,
         }
     }
 }
 
-#[derive(Debug,Clone,Default)]
+#[derive(Debug, Clone, Default)]
 pub struct RateLimiter {
-    pub(crate) rate_to_ms_conversion:i32,
-    pub(crate) consumed_tokens:i32,
-    pub(crate) last_refill_time:i64,
+    pub(crate) rate_to_ms_conversion: i32,
+    pub(crate) consumed_tokens: i32,
+    pub(crate) last_refill_time: i64,
 }
 
 impl RateLimiter {
+    pub fn load(rate_to_ms_conversion: i32, consumed_tokens: i32, last_refill_time: i64) -> Self {
+        Self {
+            rate_to_ms_conversion,
+            consumed_tokens,
+            last_refill_time,
+        }
+    }
+
     pub fn new() -> Self {
         Self::new_by_unit(RateUnit::Seconds)
     }
 
-    pub fn new_by_unit(unit:RateUnit) -> Self {
+    pub fn new_by_unit(unit: RateUnit) -> Self {
         Self::new_by_conversion(unit.get_rate_to_conversion())
     }
 
-    pub fn new_by_conversion(rate_to_ms_conversion:i32) -> Self {
-        Self { 
-            rate_to_ms_conversion, 
-            consumed_tokens: 0, 
-            last_refill_time: now_millis() as i64 
+    pub fn new_by_conversion(rate_to_ms_conversion: i32) -> Self {
+        Self {
+            rate_to_ms_conversion,
+            consumed_tokens: 0,
+            last_refill_time: now_millis() as i64,
         }
     }
 
-    pub fn acquire(&mut self,burst_size:i32,average_rate:i64) -> bool {
+    pub fn acquire(&mut self, burst_size: i32, average_rate: i64) -> bool {
         self.acquire_by_time(burst_size, average_rate, now_millis() as i64)
     }
 
-    pub fn acquire_by_time(&mut self,burst_size:i32,average_rate:i64,current_time_millis:i64) -> bool {
-        if burst_size <=0 || average_rate <=0 {
+    pub fn acquire_by_time(
+        &mut self,
+        burst_size: i32,
+        average_rate: i64,
+        current_time_millis: i64,
+    ) -> bool {
+        if burst_size <= 0 || average_rate <= 0 {
             return true;
         }
         self.refill_token(burst_size, average_rate, current_time_millis);
         self.consume_token(burst_size)
     }
 
-    fn refill_token(&mut self,burst_size:i32,average_rate:i64,current_time_millis:i64) {
+    fn refill_token(&mut self, burst_size: i32, average_rate: i64, current_time_millis: i64) {
         let time_detla = current_time_millis - self.last_refill_time;
-        let new_tokens = time_detla * average_rate /(self.rate_to_ms_conversion as i64);
-        if new_tokens <=0 {
+        let new_tokens = time_detla * average_rate / (self.rate_to_ms_conversion as i64);
+        if new_tokens <= 0 {
             return;
         }
-        self.last_refill_time = if self.last_refill_time== 0 {
+        self.last_refill_time = if self.last_refill_time == 0 {
             current_time_millis
-        }
-        else{
-            self.last_refill_time + new_tokens * (self.rate_to_ms_conversion as i64)/average_rate
+        } else {
+            self.last_refill_time + new_tokens * (self.rate_to_ms_conversion as i64) / average_rate
         };
-        let adjusted_level = cmp::min(self.consumed_tokens,burst_size);
-        self.consumed_tokens = cmp::max(0,adjusted_level - new_tokens as i32);
+        let adjusted_level = cmp::min(self.consumed_tokens, burst_size);
+        self.consumed_tokens = cmp::max(0, adjusted_level - new_tokens as i32);
     }
 
-    fn consume_token(&mut self,burst_size:i32) -> bool {
+    fn consume_token(&mut self, burst_size: i32) -> bool {
         if self.consumed_tokens >= burst_size {
             return false;
         }
@@ -83,21 +100,33 @@ impl RateLimiter {
         true
     }
 
-    pub fn reset(&mut self){
+    pub fn reset(&mut self) {
         self.consumed_tokens = 0;
         self.last_refill_time = 0;
     }
+
+    pub fn get_rate_to_ms_conversion(&self) -> i32 {
+        self.rate_to_ms_conversion
+    }
+
+    pub fn get_consumed_tokens(&self) -> i32 {
+        self.consumed_tokens
+    }
+
+    pub fn get_last_refill_time(&self) -> i64 {
+        self.last_refill_time
+    }
 }
 
-#[derive(Debug,Clone,Default)]
-pub struct QpsLimiter{
-    inner_limit:RateLimiter,
-    burst_size:i32,
-    qps_limit:i64,
+#[derive(Debug, Clone, Default)]
+pub struct QpsLimiter {
+    inner_limit: RateLimiter,
+    burst_size: i32,
+    qps_limit: i64,
 }
 
 impl QpsLimiter {
-    pub fn new(qps_limit:u64) -> Self {
+    pub fn new(qps_limit: u64) -> Self {
         Self {
             burst_size: 1,
             inner_limit: RateLimiter::new(),
@@ -105,114 +134,149 @@ impl QpsLimiter {
         }
     }
 
-    pub fn set_burst_size(mut self,burst_size:u32) -> Self{
-        self.burst_size = cmp::max(1,burst_size as i32);
+    pub fn set_burst_size(mut self, burst_size: u32) -> Self {
+        self.burst_size = cmp::max(1, burst_size as i32);
         self
     }
 
-    pub fn set_second_limit(mut self,qps_limit:u64) -> Self{
+    pub fn set_second_limit(mut self, qps_limit: u64) -> Self {
         self.qps_limit = qps_limit as i64;
         self
     }
 
     pub fn acquire(&mut self) -> bool {
-        self.inner_limit.acquire(self.burst_size , self.qps_limit)
+        self.inner_limit.acquire(self.burst_size, self.qps_limit)
     }
-    
-    pub fn reset(&mut self){
+
+    pub fn reset(&mut self) {
         self.inner_limit.reset()
     }
 }
 
-#[derive(Debug,Default)]
+#[derive(Debug, Default)]
 pub struct AtomicRateLimiter {
-    pub(crate) rate_to_ms_conversion:i32,
-    pub(crate) consumed_tokens:AtomicI32,
-    pub(crate) last_refill_time:AtomicI64,
+    pub(crate) rate_to_ms_conversion: i32,
+    pub(crate) consumed_tokens: AtomicI32,
+    pub(crate) last_refill_time: AtomicI64,
 }
 
 impl AtomicRateLimiter {
+    pub fn load(rate_to_ms_conversion: i32, consumed_tokens: i32, last_refill_time: i64) -> Self {
+        Self {
+            rate_to_ms_conversion,
+            consumed_tokens: AtomicI32::new(consumed_tokens),
+            last_refill_time: AtomicI64::new(last_refill_time),
+        }
+    }
+
     pub fn new() -> Self {
         Self::new_by_unit(RateUnit::Seconds)
     }
 
-    pub fn new_by_unit(unit:RateUnit) -> Self {
+    pub fn new_by_unit(unit: RateUnit) -> Self {
         Self::new_by_conversion(unit.get_rate_to_conversion())
     }
 
-    pub fn new_by_conversion(rate_to_ms_conversion:i32) -> Self {
-        Self { 
-            rate_to_ms_conversion, 
-            consumed_tokens: AtomicI32::new(0), 
-            last_refill_time: AtomicI64::new(now_millis() as i64)
+    pub fn new_by_conversion(rate_to_ms_conversion: i32) -> Self {
+        Self {
+            rate_to_ms_conversion,
+            consumed_tokens: AtomicI32::new(0),
+            last_refill_time: AtomicI64::new(now_millis() as i64),
         }
     }
 
-    pub fn acquire(&self,burst_size:i32,average_rate:i64) -> bool {
+    pub fn acquire(&self, burst_size: i32, average_rate: i64) -> bool {
         self.acquire_by_time(burst_size, average_rate, now_millis() as i64)
     }
 
-    pub fn acquire_by_time(&self,burst_size:i32,average_rate:i64,current_time_millis:i64) -> bool {
-        if burst_size <=0 || average_rate <=0 {
+    pub fn acquire_by_time(
+        &self,
+        burst_size: i32,
+        average_rate: i64,
+        current_time_millis: i64,
+    ) -> bool {
+        if burst_size <= 0 || average_rate <= 0 {
             return true;
         }
         self.refill_token(burst_size, average_rate, current_time_millis);
         self.consume_token(burst_size)
     }
 
-    fn refill_token(&self,burst_size:i32,average_rate:i64,current_time_millis:i64) {
+    fn refill_token(&self, burst_size: i32, average_rate: i64, current_time_millis: i64) {
         let refill_time = self.last_refill_time.load(Relaxed);
         let time_detla = current_time_millis - refill_time;
-        let new_tokens = time_detla * average_rate /(self.rate_to_ms_conversion as i64);
-        if new_tokens <=0 {
+        let new_tokens = time_detla * average_rate / (self.rate_to_ms_conversion as i64);
+        if new_tokens <= 0 {
             return;
         }
         let new_refill_time = if refill_time == 0 {
             current_time_millis
-        }
-        else{
-            refill_time + new_tokens * (self.rate_to_ms_conversion as i64)/average_rate
+        } else {
+            refill_time + new_tokens * (self.rate_to_ms_conversion as i64) / average_rate
         };
-        if self.last_refill_time.compare_exchange(refill_time, new_refill_time,Relaxed,Relaxed).is_ok() {
+        if self
+            .last_refill_time
+            .compare_exchange(refill_time, new_refill_time, Relaxed, Relaxed)
+            .is_ok()
+        {
             loop {
                 let current_level = self.consumed_tokens.load(Relaxed);
-                let adjusted_level = cmp::min(current_level,burst_size);
-                let new_level= cmp::max(0,adjusted_level - new_tokens as i32);
-                if self.consumed_tokens.compare_exchange(current_level, new_level, Relaxed, Relaxed).is_ok() {
-                    return ;
+                let adjusted_level = cmp::min(current_level, burst_size);
+                let new_level = cmp::max(0, adjusted_level - new_tokens as i32);
+                if self
+                    .consumed_tokens
+                    .compare_exchange(current_level, new_level, Relaxed, Relaxed)
+                    .is_ok()
+                {
+                    return;
                 }
             }
         }
     }
 
-    fn consume_token(&self,burst_size:i32) -> bool {
+    fn consume_token(&self, burst_size: i32) -> bool {
         loop {
             let current_level = self.consumed_tokens.load(Relaxed);
             if current_level >= burst_size {
                 return false;
             }
-            if self.consumed_tokens.compare_exchange(current_level, current_level+1, Relaxed, Relaxed).is_ok() {
+            if self
+                .consumed_tokens
+                .compare_exchange(current_level, current_level + 1, Relaxed, Relaxed)
+                .is_ok()
+            {
                 return true;
             }
         }
     }
 
-    pub fn reset(&self){
-        self.consumed_tokens.swap(0,Relaxed);
+    pub fn reset(&self) {
+        self.consumed_tokens.swap(0, Relaxed);
         self.last_refill_time.swap(0, Relaxed);
+    }
+
+    pub fn get_rate_to_ms_conversion(&self) -> i32 {
+        self.rate_to_ms_conversion
+    }
+
+    pub fn get_consumed_tokens(&self) -> i32 {
+        self.consumed_tokens.load(Relaxed)
+    }
+
+    pub fn get_last_refill_time(&self) -> i64 {
+        self.last_refill_time.load(Relaxed)
     }
 }
 
-
-#[derive(Debug,Clone,Default)]
-pub struct AtomicQpsLimiter{
-    inner_limit:Arc<AtomicRateLimiter>,
-    burst_size:i32,
-    qps_limit:i64,
+#[derive(Debug, Clone, Default)]
+pub struct AtomicQpsLimiter {
+    inner_limit: Arc<AtomicRateLimiter>,
+    burst_size: i32,
+    qps_limit: i64,
 }
 
 impl AtomicQpsLimiter {
-    pub fn new(qps_limit:u64) -> Self {
+    pub fn new(qps_limit: u64) -> Self {
         Self {
             burst_size: 1,
             inner_limit: Arc::new(AtomicRateLimiter::new()),
@@ -220,21 +284,21 @@ impl AtomicQpsLimiter {
         }
     }
 
-    pub fn set_burst_size(mut self,burst_size:u32) -> Self{
-        self.burst_size = cmp::max(1,burst_size as i32);
+    pub fn set_burst_size(mut self, burst_size: u32) -> Self {
+        self.burst_size = cmp::max(1, burst_size as i32);
         self
     }
 
-    pub fn set_second_limit(mut self,qps_limit:u64) -> Self{
+    pub fn set_second_limit(mut self, qps_limit: u64) -> Self {
         self.qps_limit = qps_limit as i64;
         self
     }
 
     pub fn acquire(&self) -> bool {
-        self.inner_limit.acquire(self.burst_size , self.qps_limit)
+        self.inner_limit.acquire(self.burst_size, self.qps_limit)
     }
-    
-    pub fn reset(&self){
+
+    pub fn reset(&self) {
         self.inner_limit.reset()
     }
 }
